e3363f36060dab842d17986f9998f06aa98817f2
Colin White <colinrtwhite@gmail.com>
2015-05-17 15:42:48 +0100

    Fix PreferenceFragment styling for pre-Lollipop.

--- app/src/main/java/com/colinwhite/ping/pref/PreferencesActivity.java
+++ app/src/main/java/com/colinwhite/ping/pref/PreferencesActivity.java
@@ -1,9 +1,12 @@
 package com.colinwhite.ping.pref;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.colinwhite.ping.R;
@@ -24,6 +27,14 @@ public class PreferencesActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
             Toast.makeText(this, getString(R.string.error), Toast.LENGTH_LONG).show();
             finish();
+        }
+
+        // Style preference ListView appropriately on pre-Lollipop.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            ListView preferences = (ListView) findViewById(android.R.id.list);
+            preferences.setSelector(R.drawable.ripple_accent);
+            preferences.setDivider(new ColorDrawable(getResources().getColor(android.R.color.darker_gray)));
+            preferences.setDividerHeight(1);
         }
     }
 
